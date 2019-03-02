6f4e18aea144e3f94caa83a3ef935177ebcaed55
bansheezzz <bansheezzz@hotmail.com>
2019-03-02 00:58:20 -0800

    Added month transition animation

--- src/components/Calendar/CalendarGrid/CalendarGrid.jsx
+++ src/components/Calendar/CalendarGrid/CalendarGrid.jsx
@@ -1,72 +1,119 @@
 import './CalendarGrid.scss';
 
 import {
+  addDays,
   eachDay,
   getDay,
   getMonth,
   getYear,
   lastDayOfMonth,
+  lastDayOfWeek,
   startOfDay,
-  addDays,
   subDays,
-  subMonths,
-  lastDayOfWeek
+  subMonths
 } from 'date-fns';
-import React from 'react';
+import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-import CalendarGridCell from './CalendarGridCell/CalendarGridCell';
 import { Validation } from '../Validation';
+import CalendarGridCell from './CalendarGridCell/CalendarGridCell';
 
 const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const renderDateCell = (date, isCurrent) => {
-  return (
-    <CalendarGridCell date={date} key={date.toString()} isCurrent={isCurrent} />
-  );
-};
+class CalendarGrid extends Component {
+  static propTypes = {
+    selectedDate: Validation.validateDate
+  };
 
-const renderWeekdayHeader = () => {
-  return WEEKDAYS.map((w, i) => <div key={i}>{w}</div>);
-};
+  renderDateCell(date, isCurrent) {
+    return (
+      <CalendarGridCell
+        date={date}
+        key={date.toString()}
+        isCurrent={isCurrent}
+      />
+    );
+  }
 
-const CalendarGrid = props => {
-  const date = startOfDay(props.selectedDate);
-  const firstDayCurrent = new Date(getYear(date), getMonth(date), 1);
-  const lastDayCurrent = lastDayOfMonth(date);
-  const weekOffset = getDay(firstDayCurrent);
-  const lastDayLast = lastDayOfMonth(subMonths(date, 1));
-  const lastDayWeek = lastDayOfWeek(lastDayCurrent);
+  renderWeekdayHeader() {
+    return WEEKDAYS.map((w, i) => <div key={i}>{w}</div>);
+  }
 
-  const priorMonth =
-    weekOffset > 0
-      ? eachDay(subDays(lastDayLast, weekOffset - 1), lastDayLast).map(d =>
-          renderDateCell(d, false)
-        )
-      : [];
-  const nextMonth =
-    getDay(lastDayCurrent) < WEEKDAYS.length - 1
-      ? eachDay(addDays(lastDayCurrent, 1), lastDayWeek).map(d =>
-          renderDateCell(d, false)
-        )
-      : [];
-  const dates = [
-    ...priorMonth,
-    ...eachDay(firstDayCurrent, lastDayCurrent).map(d =>
-      renderDateCell(d, true)
-    ),
-    ...nextMonth
-  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true
+    };
+    this.renderDateCell = this.renderDateCell.bind(this);
+    this.renderWeekdayHeader = this.renderWeekdayHeader.bind(this);
+    this.setVisible = this.setVisible.bind(this);
+  }
 
-  return (
-    <div className="CalendarGrid">
-      <div className="week">{renderWeekdayHeader()}</div>
-      <div className="dates">{dates}</div>
-    </div>
-  );
-};
+  componentDidUpdate(prevProps) {
+    const prevDate = prevProps.selectedDate;
+    const currentDate = this.props.selectedDate;
+    console.log('date', prevDate, currentDate);
+    if (prevDate !== currentDate) {
+      this.setVisible(false);
+    }
+  }
 
-CalendarGrid.propTypes = {
-  selectedDate: Validation.validateDate
-};
+  setVisible(visible) {
+    this.setState({
+      visible: visible
+    });
+  }
+
+  render() {
+    const date = startOfDay(this.props.selectedDate);
+    const firstDayCurrent = new Date(getYear(date), getMonth(date), 1);
+    const lastDayCurrent = lastDayOfMonth(date);
+    const weekOffset = getDay(firstDayCurrent);
+    const lastDayLast = lastDayOfMonth(subMonths(date, 1));
+    const lastDayWeek = lastDayOfWeek(lastDayCurrent);
+    const { visible } = { ...this.state };
+
+    const priorMonth =
+      weekOffset > 0
+        ? eachDay(subDays(lastDayLast, weekOffset - 1), lastDayLast).map(d =>
+            this.renderDateCell(d, false)
+          )
+        : [];
+    const nextMonth =
+      getDay(lastDayCurrent) < WEEKDAYS.length - 1
+        ? eachDay(addDays(lastDayCurrent, 1), lastDayWeek).map(d =>
+            this.renderDateCell(d, false)
+          )
+        : [];
+    const dates = [
+      ...priorMonth,
+      ...eachDay(firstDayCurrent, lastDayCurrent).map(d =>
+        this.renderDateCell(d, true)
+      ),
+      ...nextMonth
+    ];
+
+    const calendarGrid = visible ? (
+      <div className="CalendarGrid">
+        <div className="week">{this.renderWeekdayHeader()}</div>
+        <div className="dates">{dates}</div>
+      </div>
+    ) : (
+      <div />
+    );
+    return (
+      <CSSTransition
+        in={visible}
+        classNames="fade"
+        timeout={200}
+        onExited={() => {
+          this.setVisible(true);
+        }}
+      >
+        {calendarGrid}
+      </CSSTransition>
+    );
+  }
+}
 
 export default CalendarGrid;
